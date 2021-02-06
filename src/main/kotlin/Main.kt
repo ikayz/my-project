e457b889d0bd2f9ece097f9462c2ad65603b33cb
Isaac Miti <ikayz@live.com>
2021-02-06 14:32:46 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+/*
+Created by IntelliJ IDEA. User: ikayz Date: 06/02/2021 
+Day: Saturday Time: 2:29 pm
+*/
+fun main() {
+    person()
+}
+
+fun person() {
+    val name = "Isaac"
+    val location = "Zambia"
+    val favouriteDrink = "Apple Juice"
+
+    println("His name is $name he loves $favouriteDrink and lives in $location")
+}
